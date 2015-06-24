dbcb6eb12438e21321272cf7685210da5cb40544
Neil Leonard <najleonard@gmail.com>
2015-06-24 15:38:51 -0700

    update direction

--- bikegps.py
+++ bikegps.py
@@ -129,6 +129,9 @@ if __name__ == '__main__':
       	duration = route["rows"][0]["elements"][0]["duration"]["value"]
       	print ("Distance is %8.2fkm" % (distance/1000))
       	print ("Duraction is %8.2f minutes" % (duration/60))
+      	print ("Next direction:")
+      	directions = gmaps.directions((homePoint.latitude,homePoint.longitude),(testPoint.latitude,testPoint.longitude),mode="bicycling")
+      	print directions[0]["legs"][0]["steps"][0]["html_instructions"]
       time.sleep(5) #set to whatever
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
